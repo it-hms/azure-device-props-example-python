33a9f1ae80d5544107452d17bf723c2dfdf04862
it-hms <iatr@hms.se>
2021-06-16 16:23:19 -0400

    Change to Connections string and send msg

--- app.py
+++ app.py
@@ -2,22 +2,39 @@
 # Licensed under the MIT license.
 
 import os
-
+import random
+import json
+import time
 from azure.iot.device import IoTHubDeviceClient, Message
 
-#SAS TOKEN - Connection String is not stored in code, but read from environment
-SAS_TOKEN = os.getenv("IOTHUB_DEVICE_SAS_TOKEN", default="error")
+# Connection String is not stored in code, but read from environment
+CONN = os.getenv("IOTHUB_DEVICE_CONN_STR", default="error")
+
 
 def get_client():
-	if SAS_TOKEN == "error":
-		raise Exception("SAS Token not set in evironment")
-	return IoTHubDeviceClient.create_from_connection_string(SAS_TOKEN)
-
+    try:
+        if CONN == "error":
+            raise Exception("SAS Token not set in environment")
+        client = IoTHubDeviceClient.create_from_connection_string(CONN)
+        client.connect()
+    except Exception as e:
+        print(e)
+    return client
 
 
 def run():
-	client = get_client()
-	
+    client = get_client()
+    while True:
+        body = {"rand1": random.random() * 100, "rand2": random.random() * 100}
+        message = Message(json.dumps(body))
+        message.custom_properties["appProp1"] = "valueProp"
+        message.content_type = "application/json"
+        message.content_encoding = "utf-8"
+
+        print(f"Sending message: {message}")
+        client.send_message(message)
+        time.sleep(10)
+
 
 if __name__ == '__main__':
-	run()
+    run()
